c0ae6c25e55f93a79309e5b10d82009e41ca0ff4
Marc Johnson <marc@marcsj.com>
2019-05-25 22:34:42 -0700

    Added operations write and read for writing to a file and reading that file.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/urfave/cli"
 	"log"
 	"os"
@@ -11,15 +10,39 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "snugf"
 	app.Usage = "protect, encrypt, and compress files"
-	app.Action = run
+	app.ArgsUsage = "[input file] [output file]"
+
+	operator := operator{}
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "keyfile, K",
+			Usage: "Load key from `FILE`",
+			Destination: &operator.fileKey,
+		},
+		cli.StringFlag{
+			Name:  "key, k",
+			Usage: "Use a `string` as a key",
+			Destination: &operator.stringKey,
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "write",
+			Aliases: []string{"w"},
+			Usage:   "write a file",
+			Action:  operator.write,
+		},
+		{
+			Name:    "read",
+			Aliases: []string{"r"},
+			Usage:   "complete a task on the list",
+			Action:  operator.read,
+		},
+	}
 
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func run(c *cli.Context) error {
-	fmt.Println("Hello friend!")
-	return nil
 }
---
+++ operations.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"github.com/urfave/cli"
+	"io/ioutil"
+)
+
+type operator struct {
+	fileKey string
+	stringKey string
+}
+
+func (o operator) getKey() ([]byte, error){
+	if o.fileKey != "" {
+		file, err := ioutil.ReadFile(o.fileKey)
+		if err != nil {
+			return nil, err
+		}
+		return file, nil
+	} else if o.stringKey != "" {
+		return []byte(o.stringKey), nil
+	}
+	return nil, errors.New("no key in use")
+}
+
+func (o operator) write(c *cli.Context) error {
+	fileRead := c.Args().Get(0)
+	fileWritten := c.Args().Get(1)
+	key, err := o.getKey()
+	if err != nil {
+		return err
+	}
+
+	files, err := getFiles(fileRead)
+	if err != nil {
+		return err
+	}
+	compressedReader, err := compressEncrypt(files[0], key)
+	if err != nil {
+		return err
+	}
+	defer compressedReader.Close()
+
+	return writeFile(fileWritten, compressedReader)
+}
+
+func (o operator) read(c *cli.Context) error {
+	fileRead := c.Args().Get(0)
+	fileWritten := c.Args().Get(1)
+	key, err := o.getKey()
+	if err != nil {
+		return err
+	}
+
+	files, err := getFiles(fileRead)
+	if err != nil {
+		return err
+	}
+	compressedReader, err := decryptDecompress(files[0], key)
+	if err != nil {
+		return err
+	}
+	return writeFile(fileWritten, compressedReader)
+}
