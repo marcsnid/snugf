d42503090320add74c764aebe0b35f5cea516772
Marc Johnson <marc@marcsj.com>
2019-05-25 21:32:20 -0700

    made writeFile function to write the reader.

--- utility.go
+++ utility.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"io"
 	"os"
 )
 
@@ -19,4 +21,21 @@ func getFiles(filename string) ([]*os.File, error) {
 		return nil, err
 	}
 	return []*os.File{file}, nil
+}
+
+func writeFile(filename string, input io.Reader) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+
+	_, err = io.Copy(w, input)
+	if err != nil {
+		return err
+	}
+
+	return w.Flush()
 }
