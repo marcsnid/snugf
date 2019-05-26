8c51c16e3708aa6f06c31f3656a87952e9ef23ef
Marc Johnson <marc@marcsj.com>
2019-05-25 21:21:01 -0700

    Made simple run setup for app.

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package file_snug
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "snugf"
+	app.Usage = "protect, encrypt, and compress files"
+	app.Action = run
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	fmt.Println("Hello friend!")
+	return nil
+}
